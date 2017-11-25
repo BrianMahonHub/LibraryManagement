27152e67d77b9b3f19a7efa16f22417703badd5b
BrianMahonHub <brianmahon0101@gmail.com>
2017-11-25 23:44:47 +0000

    View Librarian GUI

---
+++ src/ViewLibrarian.java
@@ -0,0 +1,52 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
+
+public class ViewLibrarian extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ViewLibrarian frame = new ViewLibrarian();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ViewLibrarian() {
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		String data[][]=null;
+		String column[]=null;
+
+		
+		table = new JTable(data,column);
+		JScrollPane sp=new JScrollPane(table);
+		
+		contentPane.add(sp, BorderLayout.CENTER);
+	}
+
+}
