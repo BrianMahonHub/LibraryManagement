3ce0a653fe96125ea632b5de336ecc9e41254ff4
BrianMahonHub <brianmahon0101@gmail.com>
2017-11-25 23:47:45 +0000

    View Librarian Database

--- src/ViewLibrarian.java
+++ src/ViewLibrarian.java
@@ -41,7 +41,32 @@ public class ViewLibrarian extends JFrame {
 		setContentPane(contentPane);
 		String data[][]=null;
 		String column[]=null;
+		try{
+			Connection con=DB.getConnection();
+			PreparedStatement ps=con.prepareStatement("select * from librarian",ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+			ResultSet rs=ps.executeQuery();
+			
+			ResultSetMetaData rsmd=rs.getMetaData();
+			int cols=rsmd.getColumnCount();
+			column=new String[cols];
+			for(int i=1;i<=cols;i++){
+				column[i-1]=rsmd.getColumnName(i);
+			}
+			
+			rs.last();
+			int rows=rs.getRow();
+			rs.beforeFirst();
 
+			data=new String[rows][cols];
+			int count=0;
+			while(rs.next()){
+				for(int i=1;i<=cols;i++){
+					data[count][i-1]=rs.getString(i);
+				}
+				count++;
+			}
+			con.close();
+		}catch(Exception e){System.out.println(e);}
 		
 		table = new JTable(data,column);
 		JScrollPane sp=new JScrollPane(table);
