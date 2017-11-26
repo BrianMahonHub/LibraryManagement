3c2cf521454e581ed8f3b2482d6fc864a622c3ed
BrianMahonHub <brianmahon0101@gmail.com>
2017-11-26 19:20:53 +0000

    Remove Unused import

--- src/ViewLibrarian.java
+++ src/ViewLibrarian.java
@@ -7,7 +7,6 @@ import java.sql.ResultSetMetaData;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JTable;
 import javax.swing.border.LineBorder;
 import java.awt.Color;
