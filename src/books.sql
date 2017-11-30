d85b1fc71b4c25743d938acb6b2c7fb8765a9220
BrianMahonHub <brianmahon0101@gmail.com>
2017-11-30 18:43:54 +0000

    Books Database Fix

--- src/books.sql
+++ src/books.sql
@@ -4,10 +4,11 @@ CREATE TABLE IF NOT EXISTS `books` (
   `author` varchar(100) NOT NULL,
   `publisher` varchar(100) NOT NULL,
   `quantity` int(10) NOT NULL,
-  `issued` int(10) NOT NULL,
+  `issued` int(10) NULL,
   `added_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
 
 INSERT INTO `books` (`id`, `name`, `author`, `publisher`, `quantity`, `issued`, `added_date`) VALUES
 (1, 'C In Depth', 'Shrivastav', 'BPB', 2, 2, '2016-07-19 19:37:56'),
